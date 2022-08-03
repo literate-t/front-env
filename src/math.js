6ea47f4c3733c56ec4b2589ba3fcc1be113b137a
literate <gaekoon@gmail.com>
2022-08-03 12:21:05 +0900

    first commit

---
+++ src/math.js
@@ -0,0 +1,12 @@
+// // var math = math || {};
+// const math = {};
+
+// (() => {
+//   const sum = (a, b) => {
+//     return a + b;
+//   };
+
+//   math.sum = sum;
+// })();
+
+export const sum = (a, b) => a + b;
